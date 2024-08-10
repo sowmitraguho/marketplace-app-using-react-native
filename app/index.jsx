72f60230e10e4ab06eff9c290e88ea2b55bf4cc3
Sowmitra Guha <sowmmochy@gmail.com>
2024-08-10 08:40:29 +0600

    this project ends here due to older tutorial

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,13 @@
+import { StatusBar } from 'expo-status-bar'
 import { View, Text } from 'react-native'
 import React from 'react'
+import { className } from 'twrnc'
 
 const index = () => {
   return (
-    <View>
+    <View className="flex-1 items-center justify-center bg-white">
       <Text>index</Text>
+      <StatusBar style='auto'/>
     </View>
   )
 }
